6f22c3443bd407ba625a125a00e840342ceb05a8
Jonatan Vargas Ortiz <jonvargasor@gmail.com>
2024-03-03 22:04:05 -0500

    Fix: Assets

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,12 +7,12 @@
         <title>{{ $title ?? config('app.title') }}</title>
         <x-seo/>
         <link rel="shortcut icon" href="{{ config('app.favicon') ? asset('storage/'.config('app.favicon')): asset('storage/images/favicon.png') }}" type="image/png">
-        <link href="{{ Vite::asset('resources/dashboard/css/dependencies/tabler.min.css') }}" rel="stylesheet"/>
-        <link href="{{ Vite::asset('resources/dashboard/css/dependencies/tabler-flags.min.css') }}" rel="stylesheet"/>
-        <link href="{{ Vite::asset('resources/dashboard/css/dependencies/tabler-payments.min.css') }}" rel="stylesheet"/>
-        <link href="{{ Vite::asset('resources/dashboard/css/dependencies/tabler-vendors.min.css') }}" rel="stylesheet"/>
-        <link href="{{ Vite::asset('resources/dashboard/libs/flatpickr/dist/flatpickr.min.css') }}" rel="stylesheet"/>
-        <link href="{{ Vite::asset('resources/dashboard/css/dependencies/demo.min.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('storage/assets/dashboard/css/dependencies/tabler.min.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('storage/assets/dashboard/css/dependencies/tabler-flags.min.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('storage/assets/dashboard/css/dependencies/tabler-payments.min.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('storage/assets/dashboard/css/dependencies/tabler-vendors.min.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('storage/assets/dashboard/libs/flatpickr/dist/flatpickr.min.css') }}" rel="stylesheet"/>
+        <link href="{{ asset('storage/assets/dashboard/css/dependencies/demo.min.css') }}" rel="stylesheet"/>
         <link href="{{ Vite::asset('resources/dashboard/css/global.scss') }}" rel="stylesheet"/>
         @if (Route::is(['comics.*']))
             <link href="{{ Vite::asset('resources/dashboard/css/comics.scss') }}" rel="stylesheet"/>
@@ -31,23 +31,23 @@
         </style>
     </head>
     <body>
-        <script src="{{ Vite::asset('resources/dashboard/js/dependencies/demo-theme.min.js') }}"></script>
+        <script src="{{ asset('storage/assets/dashboard/js/dependencies/demo-theme.min.js') }}"></script>
         <x-dashboard.header />
         {{ $slot }}
         <x-dashboard.footer/>
         <!-- Libs JS -->
-        <script src="{{ Vite::asset('resources/dashboard/libs/apexcharts/dist/apexcharts.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/jsvectormap/dist/js/jsvectormap.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/jsvectormap/dist/maps/world.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/jsvectormap/dist/maps/world-merc.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/apexcharts/dist/apexcharts.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/jsvectormap/dist/js/jsvectormap.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/jsvectormap/dist/maps/world.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/jsvectormap/dist/maps/world-merc.js') }}" defer></script>
         <!-- Tabler Core -->
-        <script src="{{ Vite::asset('resources/dashboard/libs/list.js/dist/list.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/flatpickr/dist/flatpickr.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/litepicker/dist/litepicker.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/tom-select/dist/js/tom-select.complete.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/libs/tinymce/tinymce.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/js/dependencies/tabler.min.js') }}" defer></script>
-        <script src="{{ Vite::asset('resources/dashboard/js/dependencies/demo.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/list.js/dist/list.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/flatpickr/dist/flatpickr.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/litepicker/dist/litepicker.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/tom-select/dist/js/tom-select.complete.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/libs/tinymce/tinymce.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/js/dependencies/tabler.min.js') }}" defer></script>
+        <script src="{{ asset('storage/assets/dashboard/js/dependencies/demo.min.js') }}" defer></script>
 		
 		@if (Route::is(['comics.index', 'comics.create', 'comics.edit']))
 			<script type="module" src="{{ Vite::asset('resources/dashboard/js/comics.js') }}" defer></script>
